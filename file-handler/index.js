8cedb0f1b8879565eff372c59d4baf0801a331e3
Lior Shtivelman <lior.shtivelman@gmail.com>
2022-04-23 13:37:48 +0300

    Fix: add node_modules as dev dependencies

--- file-handler/index.js
+++ file-handler/index.js
@@ -3,7 +3,6 @@ const fs = require('fs')
 const writeLogFile = content => {
     const path = './my_log.log';
     try {
-
         fs.appendFileSync(path, content);
     } catch(err) {
         console.log(err);
